905407f9bd5e8d6812f16dbcad4d21ac5bb7394e
329439737 <329439737@qq.com>
2021-06-30 13:23:29 +0800

    11

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import{Button} from 'antd'
+
+export default class home extends Component {
+      render() {
+                            return (
+                                          <div>
+                               <div >
+     <Button type="primary">测试</Button>
+
+    </div>
+                                          </div>
+                            )
+              }
+}
